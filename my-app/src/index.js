b637db4504f47f0f87d9493515b80db4801d93ee
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-08 08:01:40 -0300

    Add prop onClick to Square component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -34,7 +34,12 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+      return (
+        <Square 
+          value = {this.state.squares[i]} 
+          onClick = {()=> this.handleClick(i)}        
+        />
+      );
     }
   
     render() {
